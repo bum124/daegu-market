abda5e07ea1c8089b125a36da3810eb8380946dc
hye70 <hye70301@google.com>
2026-04-12 15:03:50 +0900

    🚀 클라우드 DB 연동 및 mysql2 업그레이드

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
@@ -13,7 +13,8 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 // 이메일 설정 (금고에서 꺼내 쓰기)
